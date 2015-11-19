e9617bd96297731db96ac89a229b67321dfe1a3f
Adi Dobrescu <adobrescu@insolitservices.ro>
2015-11-19 13:58:55 +0200

    Modifies the constructor so it load the tokens from the source file

--- lib/PHPSource.class.php
+++ lib/PHPSource.class.php
@@ -1,11 +1,23 @@
 <?php
 
+/*
+ * PHPSource 
+ * 
+ * stie doar cateva chestii de baza legat de clase
+ * 
+ */
 class PHPSource
 {
 	protected $tokens;
 	protected $filename;
 	public function __construct($filename)
 	{
+		$this->filename=$filename;
+		$this->tokens=token_get_all(file_get_contents($this->filename));
+		foreach($this->tokens as &$token)
+		{
+			$token[3]=gettype($token[0])=='string'?'':token_name($token[0]);
+		}
 	}
 }
 
